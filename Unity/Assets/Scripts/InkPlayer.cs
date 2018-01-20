d33dc07f260e645c5c6ebd1ba2344f6bde8be6b7
Deviant Dev <dev@deviant.tech>
2018-01-21 09:07:28 +1300

    Add Utils

---
+++ Unity/Assets/Scripts/InkPlayer.cs
@@ -0,0 +1,74 @@
+﻿using Ink.Runtime;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Fuu {
+	public class InkPlayer : MonoBehaviour {
+		[SerializeField] private TextAsset m_InkJsonAsset;
+		[SerializeField] private Canvas m_Canvas;
+		[SerializeField] private Transform m_Background;
+		[SerializeField] private Transform m_LeftActor;
+		[SerializeField] private Transform m_RightActor;
+		[SerializeField] private Text m_TextPrefab;
+		[SerializeField] private Button m_ButtonPrefab;
+
+		private Story m_Story;
+
+		private void Awake() { StartStory(); }
+
+		private void StartStory() {
+			m_Story = new Story(m_InkJsonAsset.text);
+			RefreshView();
+		}
+
+		private void RefreshView() {
+			RemoveChildren();
+
+			while (m_Story.canContinue) {
+				string text = m_Story.Continue().Trim();
+				CreateContentView(text);
+			}
+
+			if (m_Story.currentChoices.Count > 0) {
+				for (int i = 0; i < m_Story.currentChoices.Count; i++) {
+					Choice choice = m_Story.currentChoices[i];
+					Button button = CreateChoiceView(choice.text.Trim());
+					button.onClick.AddListener(delegate { OnClickChoiceButton(choice); });
+				}
+			}
+			else {
+				Button choice = CreateChoiceView("End of story.\nRestart?");
+				choice.onClick.AddListener(delegate { StartStory(); });
+			}
+		}
+
+		private void OnClickChoiceButton(Choice choice) {
+			m_Story.ChooseChoiceIndex(choice.index);
+			RefreshView();
+		}
+
+		private void CreateContentView(string text) {
+			Text storyText = Instantiate(m_TextPrefab);
+			storyText.text = text;
+			storyText.transform.SetParent(m_Canvas.transform, false);
+		}
+
+		private Button CreateChoiceView(string text) {
+			Button choice = Instantiate(m_ButtonPrefab);
+			choice.transform.SetParent(m_Canvas.transform, false);
+
+			Text choiceText = choice.GetComponentInChildren<Text>();
+			choiceText.text = text;
+
+			HorizontalLayoutGroup layoutGroup = choice.GetComponent<HorizontalLayoutGroup>();
+			layoutGroup.childForceExpandHeight = false;
+
+			return choice;
+		}
+
+		private void RemoveChildren() {
+			int childCount = m_Canvas.transform.childCount;
+			for (int i = childCount - 1; i >= 0; --i) { Destroy(m_Canvas.transform.GetChild(i).gameObject); }
+		}
+	}
+}
---
+++ Unity/Assets/Scripts/Utils/Pulse.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Deviant.Utils {
+	[RequireComponent(typeof(Graphic))]
+	public class Pulse : MonoBehaviour {
+		[SerializeField] private AnimationCurve m_Curve = AnimationCurve.EaseInOut(0, 1, 1, 0);
+		[SerializeField] private Graphic m_Graphic;
+
+		private Color m_StartColor;
+
+		private void Start() {
+			m_Graphic = m_Graphic ? m_Graphic : (m_Graphic = GetComponent<Graphic>());
+			if (m_Graphic) { m_StartColor = m_Graphic.color; }
+			if (m_Curve.postWrapMode != WrapMode.Loop && m_Curve.postWrapMode != WrapMode.PingPong) { m_Curve.postWrapMode = WrapMode.PingPong; }
+		}
+
+		private void Update() {
+			if (m_Graphic) { m_Graphic.color = new Color(m_StartColor.r, m_StartColor.g, m_StartColor.b, m_Curve.Evaluate(Time.time)); }
+		}
+	}
+}
