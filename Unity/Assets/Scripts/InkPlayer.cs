43e414e296236db484605e42ac30f15e2f0fee38
Deviant Dev <dev@deviant.tech>
2018-01-21 15:28:18 +1300

    Update UI transition and text formatting

--- Unity/Assets/Scripts/InkPlayer.cs
+++ Unity/Assets/Scripts/InkPlayer.cs
@@ -1,16 +1,30 @@
-﻿using Ink.Runtime;
+﻿using Deviant.Utils;
+using Ink.Runtime;
 using UnityEngine;
 using UnityEngine.UI;
 
 namespace Fuu {
 	public class InkPlayer : MonoBehaviour {
 		[SerializeField] private TextAsset m_InkJsonAsset;
+
+		[Header("Settings")]
+
+		[SerializeField] private float m_NextDelay = 1;
+
+		[Header("UI")]
+
 		[SerializeField] private Canvas m_Canvas;
-		[SerializeField] private Transform m_Background;
-		[SerializeField] private Transform m_LeftActor;
-		[SerializeField] private Transform m_RightActor;
 		[SerializeField] private Text m_TextPrefab;
 		[SerializeField] private Button m_ButtonPrefab;
+
+		[Header("Stage")]
+
+		[SerializeField] private Transform m_Backdrop;
+		[SerializeField] private Transform m_OnStageLeft;
+		[SerializeField] private Transform m_OnStageRight;
+		[SerializeField] private Transform m_OffStageLeft;
+		[SerializeField] private Transform m_OffStageRight;
+
 
 		private Story m_Story;
 
@@ -18,15 +32,20 @@ namespace Fuu {
 
 		private void StartStory() {
 			m_Story = new Story(m_InkJsonAsset.text);
-			RefreshView();
+			Refresh();
 		}
 
-		private void RefreshView() {
-			RemoveChildren();
+		private void Refresh() {
+			m_Canvas.transform.DestroyAllChildren();
+			Next();
+		}
 
-			while (m_Story.canContinue) {
+		private void Next() {
+			if (m_Story.canContinue) {
 				string text = m_Story.Continue().Trim();
 				CreateContentView(text);
+				DelayTracker.DelayAction(m_NextDelay, Next);
+				return;
 			}
 
 			if (m_Story.currentChoices.Count > 0) {
@@ -38,19 +57,28 @@ namespace Fuu {
 			}
 			else {
 				Button choice = CreateChoiceView("End of story.\nRestart?");
-				choice.onClick.AddListener(delegate { StartStory(); });
+				choice.onClick.AddListener(StartStory);
 			}
 		}
 
 		private void OnClickChoiceButton(Choice choice) {
 			m_Story.ChooseChoiceIndex(choice.index);
-			RefreshView();
+			Refresh();
 		}
 
 		private void CreateContentView(string text) {
 			Text storyText = Instantiate(m_TextPrefab);
-			storyText.text = text;
+			storyText.text = CleanText(text);
 			storyText.transform.SetParent(m_Canvas.transform, false);
+		}
+
+		private static string CleanText(string text) {
+			if (text.Contains("<i>") && !text.Contains("</i>")) { text += "</i>"; }
+			text = text.ReplaceRegex(@"\<h3\>", "<size=30><b>");
+			text = text.ReplaceRegex(@"\<h4\>", "<size=25><b>");
+			text = text.ReplaceRegex(@"\</h[34]\>", "</b></size>");
+			text = text.ReplaceRegex(@"\</?h[0-9]\>", "");
+			return text;
 		}
 
 		private Button CreateChoiceView(string text) {
@@ -58,17 +86,12 @@ namespace Fuu {
 			choice.transform.SetParent(m_Canvas.transform, false);
 
 			Text choiceText = choice.GetComponentInChildren<Text>();
-			choiceText.text = text;
+			choiceText.text = CleanText(text);
 
 			HorizontalLayoutGroup layoutGroup = choice.GetComponent<HorizontalLayoutGroup>();
 			layoutGroup.childForceExpandHeight = false;
 
 			return choice;
-		}
-
-		private void RemoveChildren() {
-			int childCount = m_Canvas.transform.childCount;
-			for (int i = childCount - 1; i >= 0; --i) { Destroy(m_Canvas.transform.GetChild(i).gameObject); }
 		}
 	}
 }
---
+++ Unity/Assets/Scripts/Reveal.cs
@@ -0,0 +1,21 @@
+﻿using DG.Tweening;
+using UnityEngine;
+
+public class Reveal : MonoBehaviour {
+	[SerializeField] private CanvasRenderer m_Renderer;
+	[SerializeField] private float m_ScaleDuration = 0.2f;
+	[SerializeField] private float m_FadeDuration = 0.8f;
+
+	private Vector3 m_TargetScale;
+	private void Awake() { m_TargetScale = transform.localScale; }
+
+	private void OnEnable() {
+		transform.localScale = new Vector3(transform.localScale.x, 0, transform.localScale.z);
+		m_Renderer.SetAlpha(0);
+		transform.DOScale(m_TargetScale, m_ScaleDuration).OnComplete(FadeIn);
+	}
+
+	private void FadeIn() {
+		DOTween.To(() => m_Renderer.GetAlpha(), m_Renderer.SetAlpha, 1, m_FadeDuration);
+	}
+}
