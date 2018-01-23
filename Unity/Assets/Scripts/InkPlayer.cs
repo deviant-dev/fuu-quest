4fffa1838dbf1eef42a92488204d84e056a68e37
Deviant Dev <dev@deviant.tech>
2018-01-24 02:02:43 +1300

    Update interface and publish to WebGL

--- Unity/Assets/Scripts/InkPlayer.cs
+++ Unity/Assets/Scripts/InkPlayer.cs
@@ -4,7 +4,6 @@ using Deviant.Utils;
 using DG.Tweening;
 using Ink.Runtime;
 using UnityEngine;
-using UnityEngine.UI;
 
 namespace Fuu {
 	public class InkPlayer : MonoBehaviour {
@@ -18,13 +17,12 @@ namespace Fuu {
 		[Header("UI")]
 
 		[SerializeField] private Canvas m_Canvas;
-		[SerializeField] private VerticalLayoutGroup m_LayoutGroup;
 		[SerializeField] private Line m_TitleLinePrefab;
 		[SerializeField] private Line m_SubtitleLinePrefab;
-		[SerializeField] private Line m_DefaultTextPrefab;
-		[SerializeField] private Line m_NPCTextPrefab;
-		[SerializeField] private Line m_PlayerTextPrefab;
-		[SerializeField] private Button m_ButtonPrefab;
+		[SerializeField] private Line m_DefaultLinePrefab;
+		[SerializeField] private Line m_NPCLinePrefab;
+		[SerializeField] private Line m_PlayerLinePrefab;
+		[SerializeField] private ChoiceLine m_PlayerChoicePrefab;
 
 		[Header("Stage")]
 
@@ -36,7 +34,9 @@ namespace Fuu {
 		[SerializeField] private float m_SlideDuration = 1;
 
 		private Story m_Story;
-		private bool m_FirstLine;
+		private int m_ResponseIndex;
+		private string m_RightCharacter;
+		private string m_LeftCharacter;
 
 		private void OnEnable() { StartStory(); }
 
@@ -61,21 +61,60 @@ namespace Fuu {
 			Next();
 		}
 
-		private string GetTagValue(string tagKey) {
+		private bool GetTagValue(string tagKey, out string tagValue) {
 			string tagString = m_Story.currentTags.FirstOrDefault(t => t.StartsWith(tagKey + ":", StringComparison.OrdinalIgnoreCase));
-			return tag.IsNullOrEmpty() ? null : tag.Split(':').ElementAtOrDefault(1);
+			if (tagString.IsNullOrEmpty()) {
+				tagValue = null;
+				return false;
+			}
+			tagValue = tagString.Split(':').ElementAtOrDefault(1);
+			return true;
 		}
 
 		private void Next() {
 			if (m_Story.canContinue) {
 				string text = m_Story.Continue().Trim();
-				Line nextLine = CreateContentView(text, m_FirstLine);
-				m_FirstLine = false;
+				Line nextLine = CreateContentView(text, m_ResponseIndex);
+				m_ResponseIndex = -1;
+
+				string back;
+				string right;
+				string left;
 
 				// Collect possible commands.
-				string back = GetTagValue("back");
-				string right = GetTagValue("right");
-				string left = GetTagValue("left");
+				if (GetTagValue("back", out back)) {
+					m_Backdrop.DestroyAllChildren();
+					if (m_Assets.Backdrops.ContainsKey(back)) { Instantiate(m_Assets.Backdrops[back], m_Backdrop, false); }
+				}
+
+				if (GetTagValue("right", out right)) {
+					m_OnStageRight.DestroyAllChildren();
+					if (m_Assets.Characters.ContainsKey(right)) {
+						GameObject go = Instantiate(m_Assets.Characters[right], m_OnStageRight, false);
+						go.transform.position = m_OffStageRight.position;
+						go.transform.DOLocalMove(Vector3.zero, m_SlideDuration);
+					}
+				}
+
+				if (GetTagValue("left", out left)) {
+					m_OnStageLeft.DestroyAllChildren();
+					if (m_Assets.Characters.ContainsKey(left)) {
+						GameObject go = Instantiate(m_Assets.Characters[left], m_OnStageLeft, false);
+						go.transform.position = m_OffStageLeft.position;
+						go.transform.DOLocalMove(Vector3.zero, m_SlideDuration);
+					}
+				}
+
+				TextFade tf = nextLine.GetComponentInChildren<TextFade>();
+
+				if (tf) { tf.OnComplete.AddListener(Next); }
+				else { DelayTracker.DelayAction(m_NextDelay, Next); }
+
+				return;
+
+				// Skip showing characters if they are already there.
+				if (right == m_RightCharacter) right = null;
+				if (left == m_LeftCharacter) left = null;
 
 				Sequence sequence = DOTween.Sequence();
 
@@ -98,14 +137,20 @@ namespace Fuu {
 					if (!left.IsNullOrEmpty()) { sequence.Append(ClearStageLeft()); }
 				}
 
+				sequence.AppendInterval(0.2f);
+
 				// Handle right character returning.
 				if (!right.IsNullOrEmpty()) {
-					sequence.Append(EnterCharacter(right, m_OnStageRight, m_OffStageRight));
+					Debug.Log("Adding character creation callback for " + right + " on right side.");
+					sequence.AppendCallback(() => EnterCharacter(right, m_OnStageRight, m_OffStageRight));
+					m_RightCharacter = right;
 				}
 
 				// Handle left character returning.
 				if (!left.IsNullOrEmpty()) {
-					sequence.Append(EnterCharacter(left, m_OnStageLeft, m_OffStageLeft));
+					Debug.Log("Adding character creation callback for " + left + " on left side.");
+					sequence.AppendCallback(() => EnterCharacter(left, m_OnStageLeft, m_OffStageLeft));
+					m_LeftCharacter = left;
 				}
 
 				TextFade textFade = nextLine.GetComponentInChildren<TextFade>();
@@ -119,12 +164,12 @@ namespace Fuu {
 			if (m_Story.currentChoices.Count > 0) {
 				for (int i = 0; i < m_Story.currentChoices.Count; i++) {
 					Choice choice = m_Story.currentChoices[i];
-					Button button = CreateChoiceView(choice.text.Trim());
-					button.onClick.AddListener(() => OnClickChoiceButton(button, choice));
+					ChoiceLine button = CreateChoiceView(choice.text.Trim());
+					button.OnClick.AddListener(() => OnClickChoiceButton(button, choice));
 				}
 			} else {
-				Button choice = CreateChoiceView("End of story.\nRestart?");
-				choice.onClick.AddListener(StartStory);
+				ChoiceLine choice = CreateChoiceView("End of story.\nRestart?");
+				choice.OnClick.AddListener(StartStory);
 			}
 		}
 
@@ -136,50 +181,57 @@ namespace Fuu {
 		private Tween ClearStageSide(Transform onStage, Transform offStage) {
 			if (onStage.childCount == 1) {
 				Transform child = onStage.GetChild(0);
-				return child.DOMove(offStage.position, m_SlideDuration).SetEase(Ease.InSine);
+				return child.DOMove(offStage.position, m_SlideDuration).SetEase(Ease.InSine).OnComplete(() => UnityUtils.DestroyObject(child));
 			}
 
-			return DOTween.Sequence();
+			Debug.Log("No one to move. Returning an empty sequence.");
+			return DOTween.Sequence().OnComplete(() => Debug.Log("Empty sequence is done."));
 		}
 
-		private Tween EnterCharacter(string character, Transform onStage, Transform offStage) {
-			onStage.DestroyAllChildren();
-
+		private void EnterCharacter(string character, Transform onStage, Transform offStage) {
+			Debug.Log("Trying to create " + character + " in " + onStage);
 			if (m_Assets.Characters.ContainsKey(character)) {
 				GameObject go = Instantiate(m_Assets.Characters[character], onStage);
 				go.transform.ResetTransform();
 				go.transform.position = offStage.position;
-				return go.transform.DOLocalMove(Vector3.zero, m_SlideDuration).SetEase(Ease.OutSine);
+				go.transform.DOLocalMove(Vector3.zero, m_SlideDuration).SetEase(Ease.OutSine);
+				Debug.Log("Created " + character + " in " + onStage);
 			}
-
-			Debug.LogWarning("Can't find character '" + character + "'", this);
-			return DOTween.Sequence();
+			else {
+				Debug.LogWarning("Can't find character '" + character + "'", this);
+			}
 		}
 
-		private void OnClickChoiceButton(Button sender, Choice choice) {
+		private void OnClickChoiceButton(ChoiceLine sender, Choice choice) {
+			m_ResponseIndex = sender.transform.GetSiblingIndex();
 			UnityUtils.DestroyObject(sender);
 			m_Story.ChooseChoiceIndex(choice.index);
-			m_FirstLine = true;
 			Refresh();
 		}
 
-		private Line CreateContentView(string text, bool skipTransition = false) {
+		private Line CreateContentView(string text, int insertIndex = -1) {
 			Line linePrefab = GetLinePrefab(text);
 			Line storyLine = Instantiate(linePrefab, m_Canvas.transform, false);
-			storyLine.SetText(text, skipTransition);
+			storyLine.SetText(text, insertIndex >= 0);
+
+			if (insertIndex >= 0 && linePrefab == m_PlayerLinePrefab) {
+				storyLine.transform.SetSiblingIndex(insertIndex);
+				DelayTracker.DelayAction(5, () => storyLine.Fade());
+			}
+
 			return storyLine;
 		}
 
 		private Line GetLinePrefab(string text) {
 			if (text.Contains("<h4")) { return m_SubtitleLinePrefab; }
 			if (text.Contains("<h")) { return m_TitleLinePrefab; }
-			if (text.StartsWith("(") || m_Story.currentTags.Contains("desc") || m_Story.currentTags.Contains("writing")) { return m_DefaultTextPrefab; }
-			if (text.Contains("<i>")) { return m_PlayerTextPrefab; }
-			return m_NPCTextPrefab;
+			if (text.StartsWith("(") || m_Story.currentTags.Contains("desc") || m_Story.currentTags.Contains("writing")) { return m_DefaultLinePrefab; }
+			if (text.Contains("<i>")) { return m_PlayerLinePrefab; }
+			return m_NPCLinePrefab;
 		}
 
-		private Button CreateChoiceView(string text) {
-			Button choice = Instantiate(m_ButtonPrefab, m_Canvas.transform, false);
+		private ChoiceLine CreateChoiceView(string text) {
+			ChoiceLine choice = Instantiate(m_PlayerChoicePrefab, m_Canvas.transform, false);
 			Line line = choice.GetComponent<Line>();
 			line.SetText(text);
 			return choice;
--- Unity/Assets/Scripts/Line.cs
+++ Unity/Assets/Scripts/Line.cs
@@ -1,57 +1,73 @@
-﻿using System;
+﻿using System.Linq;
 using Deviant.Utils;
 using DG.Tweening;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class Line : MonoBehaviour {
-	[SerializeField] private Text m_Text;
-	[SerializeField] private CanvasRenderer m_TextRenderer;
-	[SerializeField] private CanvasRenderer m_BackgroundRenderer;
-	[SerializeField] private LayoutGroup m_LayoutGroup;
-	[SerializeField] private float m_FadeDuration = 1;
-	[SerializeField] private int m_ScrollSpeed = 2;
-	[SerializeField] private int m_HiddenBottomPadding = -10;
+namespace Fuu {
+	public class Line : MonoBehaviour {
+		[SerializeField] private float m_FadeDuration = 1;
+		[SerializeField] private int m_ScrollSpeed = 2;
+		[SerializeField] private int m_HiddenBottomPadding = -10;
 
-	private int m_TargetBottom;
-	private float m_Alpha;
-	private bool m_Skip;
+		private CanvasRenderer m_BackgroundRenderer;
+		private Text m_Text;
+		private CanvasRenderer m_TextRenderer;
+		private LayoutGroup m_LayoutGroup;
+		private int m_TargetBottom;
+		private float m_Alpha;
+		private bool m_Skip;
 
-	private float Alpha {
-		get { return m_Alpha; }
-		set {
-			m_Alpha = Mathf.Clamp01(value);
-			m_TextRenderer.SetAlpha(m_Alpha);
-			if (m_BackgroundRenderer) { m_BackgroundRenderer.SetAlpha(m_Alpha); }
+		private float Alpha {
+			get { return m_Alpha; }
+			set {
+				m_Alpha = Mathf.Clamp01(value);
+				m_TextRenderer.SetAlpha(m_Alpha);
+				if (m_BackgroundRenderer) { m_BackgroundRenderer.SetAlpha(m_Alpha); }
+			}
 		}
-	}
 
-	private void Start() {
-		if (m_Skip) { return; }
-		m_TargetBottom = m_LayoutGroup.padding.bottom;
-		m_LayoutGroup.padding.bottom = m_HiddenBottomPadding;
-		m_TextRenderer.SetAlpha(0);
-		if (m_BackgroundRenderer) { m_BackgroundRenderer.SetAlpha(0); }
-	}
-
-	private void LateUpdate() {
-		if (m_Skip) { return; }
-
-		if (m_LayoutGroup.padding.bottom < m_TargetBottom) {
-			m_LayoutGroup.padding.bottom = Mathf.Min(m_LayoutGroup.padding.bottom + m_ScrollSpeed, m_TargetBottom);
+		protected virtual void Awake() {
+			m_Text = GetComponentInChildren<Text>();
+			m_TextRenderer = m_Text.GetComponent<CanvasRenderer>();
+			m_BackgroundRenderer = GetComponentsInChildren<CanvasRenderer>().LastOrDefault(r => r != m_TextRenderer);
+			m_LayoutGroup = GetComponent<LayoutGroup>();
 		}
-		else if (Alpha < 1) {
-			Alpha += Time.deltaTime * m_FadeDuration;
+
+		private void Start() {
+			if (m_Skip) { return; }
+			m_TargetBottom = m_LayoutGroup.padding.bottom;
+			m_LayoutGroup.padding.bottom = m_HiddenBottomPadding;
+			m_TextRenderer.SetAlpha(0);
+			if (m_BackgroundRenderer) { m_BackgroundRenderer.SetAlpha(0); }
 		}
-		else { m_Skip = true; }
+
+		private void LateUpdate() {
+			if (m_Skip) { return; }
+
+			if (m_LayoutGroup.padding.bottom < m_TargetBottom) {
+				m_LayoutGroup.padding.bottom = Mathf.Min(m_LayoutGroup.padding.bottom + m_ScrollSpeed, m_TargetBottom);
+			}
+			else if (Alpha < 1) {
+				Alpha += Time.deltaTime * m_FadeDuration;
+			}
+			else { m_Skip = true; }
+		}
+
+		public void SetText(string text, bool skipTransition = false) {
+			m_Text.text = CleanText(text);
+			m_Skip = skipTransition;
+		}
+
+		public Tween Fade() {
+			m_Skip = true;
+			Alpha = 1;
+			Sequence sequence = DOTween.Sequence();
+			sequence.Append(DOTween.To(() => Alpha, v => Alpha = v, 0, 0.5f).OnComplete(() => UnityUtils.DestroyObject(this)));
+			sequence.Append(DOTween.To(() => m_LayoutGroup.padding.bottom, (v) => m_LayoutGroup.padding.bottom = v, m_HiddenBottomPadding, 0.5f));
+			return sequence;
+		}
+
+		private static string CleanText(string text) { return text.ReplaceRegex(@"\<[^>]+\>", ""); }
 	}
-
-	public void SetText(string text, bool skipTransition = false) {
-		m_Text.text = CleanText(text);
-		m_Skip = skipTransition;
-	}
-
-	public Tween Fade() { return DOTween.To(() => Alpha, v => Alpha = v, 0, m_FadeDuration).OnComplete(() => UnityUtils.DestroyObject(this)); }
-
-	private static string CleanText(string text) { return text.ReplaceRegex(@"\<[^>]+\>", ""); }
 }
